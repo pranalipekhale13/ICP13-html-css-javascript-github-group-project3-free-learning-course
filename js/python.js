046233bab7b5098e04e645ad62bdc7cb63e3a757
Dipti17Bhawar <diptibhawar@gmail.com>
2026-01-05 22:45:25 +0530

    add js to python

---
+++ js/python.js
@@ -0,0 +1,10 @@
+function showTab(tabId) {
+    const contents = document.querySelectorAll('.tab-content');
+    const tabs = document.querySelectorAll('.tab');
+
+    contents.forEach(c => c.classList.remove('active'));
+    tabs.forEach(t => t.classList.remove('active'));
+
+    document.getElementById(tabId).classList.add('active');
+    event.target.classList.add('active');
+}
