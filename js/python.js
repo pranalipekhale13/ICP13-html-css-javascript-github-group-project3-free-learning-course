7e4dc752313b140a5a3455dc21abc8cf73f0bbcf
Dipti17Bhawar <diptibhawar@gmail.com>
2026-01-08 15:17:03 +0530

    add changes to python course

--- js/python.js
+++ js/python.js
@@ -1,10 +1,17 @@
-function showTab(tabId) {
-    const contents = document.querySelectorAll('.tab-content');
-    const tabs = document.querySelectorAll('.tab');
+function showSection(id) {
+  document.querySelectorAll('.section').forEach(sec => {
+    sec.classList.remove('active');
+  });
 
-    contents.forEach(c => c.classList.remove('active'));
-    tabs.forEach(t => t.classList.remove('active'));
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
 
-    document.getElementById(tabId).classList.add('active');
-    event.target.classList.add('active');
+  document.getElementById(id).classList.add('active');
+  event.target.classList.add('active');
+}
+
+function toggleAccordion(header) {
+  const body = header.nextElementSibling;
+  body.style.display = body.style.display === "block" ? "none" : "block";
 }
