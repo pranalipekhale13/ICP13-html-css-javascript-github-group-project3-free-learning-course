0ee01b3a63d4692b494bdb1eb938966e1c2c88a4
Dipti17Bhawar <diptibhawar@gmail.com>
2026-01-15 21:52:58 +0530

    changes color

--- js/Ai.js
+++ js/Ai.js
@@ -15,15 +15,26 @@ function toggleAccordion(header) {
   const body = header.nextElementSibling;
   body.style.display = body.style.display === "block" ? "none" : "block";
 }
-<<<<<<< HEAD
+
+
+function showSection(id) {
+  document.querySelectorAll('.section').forEach(sec => {
+    sec.classList.remove('active');
+  });
+
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  document.getElementById(id).classList.add('active');
+  event.target.classList.add('active');
+}
+
+function toggleAccordion(header) {
+  const body = header.nextElementSibling;
+  body.style.display = body.style.display === "block" ? "none" : "block";
+}
 function toggleMenu() {
         document.querySelector(".navbar").classList.toggle("show");
     }
-=======
-
-
-
-
-
->>>>>>> 226808ee55c5c65da23835226ea8bb03b1146aa1
 
