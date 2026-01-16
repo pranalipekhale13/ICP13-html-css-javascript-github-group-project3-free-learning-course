77c90cfcc0610f1359693b66d9923178b57bf1b2
Dipti17Bhawar <diptibhawar@gmail.com>
2026-01-16 10:56:43 +0530

    js logic

--- js/Ai.js
+++ js/Ai.js
@@ -15,32 +15,7 @@ function toggleAccordion(header) {
   const body = header.nextElementSibling;
   body.style.display = body.style.display === "block" ? "none" : "block";
 }
-
-<<<<<<< HEAD
-
-function showSection(id) {
-  document.querySelectorAll('.section').forEach(sec => {
-    sec.classList.remove('active');
-  });
-
-  document.querySelectorAll('.tab').forEach(tab => {
-    tab.classList.remove('active');
-  });
-
-  document.getElementById(id).classList.add('active');
-  event.target.classList.add('active');
-}
-
-function toggleAccordion(header) {
-  const body = header.nextElementSibling;
-  body.style.display = body.style.display === "block" ? "none" : "block";
-}
 function toggleMenu() {
         document.querySelector(".navbar").classList.toggle("show");
     }
-=======
-function toggleMenu() {
-        document.querySelector(".navbar").classList.toggle("show");
-}
->>>>>>> 67943b8c75243cab5571fc82358a0737281011f0
 
